c61db9190bb27bee5360183af600d15525327c7d
Adrien Ruault <ruaultadrien@gmail.com>
2025-07-19 10:49:28 +0200

    Add browser functionalities from helium and selenium

--- src/agents.py
+++ src/agents.py
@@ -1,7 +1,14 @@
 """Agents module."""
 
-from smolagents import CodeAgent, GoogleSearchTool, Model, VisitWebpageTool
+from smolagents import (
+    CodeAgent,
+    DuckDuckGoSearchTool,
+    GoogleSearchTool,
+    Model,
+    VisitWebpageTool,
+)
 
+from src.browser_tools import close_popups, go_back, save_screenshot, search_item_ctrl_f
 from src.tools import calculate_cargo_travel_time
 
 
@@ -19,4 +26,16 @@ def get_web_agent(model: Model) -> CodeAgent:
         max_steps=10,
         verbosity_level=0,
         add_base_tools=False,
+    )
+
+
+def get_browser_agent(model: Model) -> CodeAgent:
+    """Initialize the CodeAgent with the specified model."""
+    return CodeAgent(
+        tools=[DuckDuckGoSearchTool(), go_back, close_popups, search_item_ctrl_f],
+        model=model,
+        additional_authorized_imports=["helium"],
+        step_callbacks=[save_screenshot],
+        max_steps=20,
+        verbosity_level=2,
     )
